1bdc3b76a7969d507d23ea932f92ff445c4d92bd
akraradets <akraradets@gmail.com>
2024-01-24 12:56:08 +0700

    add function in lexer

--- src/components/lexica.py
+++ src/components/lexica.py
@@ -14,6 +14,9 @@ class MyLexer(Lexer):
     MyLexer is a class that inherits from sly.Lexer
     It is used to tokenize the input string.
     ref: https://sly.readthedocs.io/en/latest/sly.html#sly-sly-lex-yacc
+    
+
+    Python regEX: https://www.w3schools.com/python/python_regex.asp
     """
 
     ### `tokens` ###
@@ -23,14 +26,15 @@ class MyLexer(Lexer):
     tokens = { NAME, NUMBER, PLUS, TIMES, MINUS, DIVIDE, 
               ASSIGN, LPAREN, RPAREN }
     
+    # https://sly.readthedocs.io/en/latest/sly.html#literal-characters
+    literals = { '+','-','*','/','(',')' }
+    
     ### matching rule ###
     # The matching work from top to bottom
     # At least, all toekns must be defined here
 
     # Ignore spaces and tabs 
     ignore = ' \t'
-    # Ignored pattern
-    ignore_newline = r'\n+'
 
     ### EX1: simply define with regEX ###
     NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
@@ -40,31 +44,33 @@ class MyLexer(Lexer):
         # Note that this function set parse token.value to integer
         token.value = int(token.value)
         # Extra print for debug
-        print(f"This print from NUMBER function: {token.type=} {token.value=} {type(token.value)=}")
+        print(f"This print from NUMBER function: {token.type=} {token.value=} {type(token.value)=}\n")
         return token
 
-
-    PLUS = r'\+'
-    MINUS = r'-'
-    TIMES = r'\*'
-    DIVIDE = r'/'
+    # Try uncomment this and run to see the differences between `token` and `literal`
+    # PLUS = r'\+'
+    # MINUS = r'-'
+    # TIMES = r'\*'
+    # DIVIDE = r'/'
+    # LPAREN = r'\('
+    # RPAREN = r'\)'
     ASSIGN = r'='
-    LPAREN = r'\('
-    RPAREN = r'\)'
 
     # Extra action for newlines
+    @_(r'\n+')
     def ignore_newline(self, t):
+        # https://sly.readthedocs.io/en/latest/sly.html#line-numbers-and-position-tracking
         self.lineno += t.value.count('\n')
 
     def error(self, t):
-        print("Illegal character '%s'" % t.value[0])
         self.index += 1
+        print(f"ERROR: Illegal character '{t.value[0]}' at line {self.lineno}")
 
 if __name__ == '__main__':
     # Write a simple test that only run when you execute this file
-    string_input:str = "1 + 1as"
+    string_input:str = "x1 + 1as!"
     lex:Lexer = MyLexer()
     # assign type to `token`
     token: sly.lex.Token
     for token in lex.tokenize(string_input):
-        print(f"{token.type=} {token.value=} {type(token.value)=}")
+        print(f"{token.type=} {token.value=} {type(token.value)=}\n")
